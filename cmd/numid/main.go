dba1e4359d0990ffb15247d572680163b0b6965c
Developer Experience team at Tendermint <hello@tendermint.com>
2022-11-19 13:31:13 -0800

    Initialized with Ignite CLI

---
+++ cmd/numid/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"os"
+
+	"github.com/cosmos/cosmos-sdk/server"
+	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
+
+	"github.com/johnreitano/numi/app"
+	"github.com/johnreitano/numi/cmd/numid/cmd"
+)
+
+func main() {
+	rootCmd, _ := cmd.NewRootCmd()
+	if err := svrcmd.Execute(rootCmd, "", app.DefaultNodeHome); err != nil {
+		switch e := err.(type) {
+		case server.ErrorCode:
+			os.Exit(e.Code)
+
+		default:
+			os.Exit(1)
+		}
+	}
+}
